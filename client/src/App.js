43005f6c03fca46bff7b36e52e12c7732785141a
Beth Damato <bethadamato@gmail.com>
2020-09-29 12:00:05 -0400

    adds positioning on vertical tabs

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,30 @@
 import React, { Component } from 'react';
 import Navbar from './components/Navbar';
 import Verttabs from './components/Verttabs'
+import CssBaseline from '@material-ui/core/CssBaseline';
+import Container from '@material-ui/core/Container';
+import { withStyles } from '@material-ui/core/styles';
 
-import './styles/styles.css'
+
+const styles = theme => ({
+  root: {
+      marginTop: 120,
+  }
+});
 
 class App extends Component {
   render() {
+    const { classes } = this.props;
     return (
-      <div className="App">
+      <div className={classes.root}>
         <Navbar />
-        <Verttabs />
+        <CssBaseline />
+        <Container maxWidth="lg">
+          <Verttabs />
+        </Container>
       </div>
     );
   }
 }
 
-export default App;
+export default withStyles(styles)(App);
