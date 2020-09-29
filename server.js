e5413dca11052a6ab0b1a495f5c2b1d2963af074
Beth Damato <bethadamato@gmail.com>
2020-09-28 21:08:53 -0400

    uses tutorial to add create a get request using a centralized query file.  Also adds frontend UI files via create-react-app

--- server.js
+++ server.js
@@ -1,9 +1,12 @@
 const express = require('express');
+const cors = require('cors')
 const bodyParser = require('body-parser');
-const { response } = require('express');
+// const { response } = require('express');
 const app = express();
 const db = require('./queries')
 const port = 3000;
+
+app.use(cors())
 
 app.use(bodyParser.json());
 app.use(
@@ -12,16 +15,13 @@ app.use(
   })
 );
 
-
+//initial connect to backend
 app.get('/api', (req, res) => {
   res.json({info: 'Node.js, Express, and Postgres API is connected!'})
 });
 
-
+//query for getting first 10 customers
 app.get('/api/customers', db.getCustomers)
-
-
-
 
 
 
